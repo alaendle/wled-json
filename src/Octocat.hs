8fd9896571dbb48de0ac7bebacf671dd9bf1b669
Andreas Ländle <alaendle@users.noreply.github.com>
2024-11-12 19:30:58 +0100

    Modeled segments.

---
+++ src/Octocat.hs
@@ -0,0 +1,15 @@
+module Octocat(france) where
+
+import Types
+
+france :: StatePatch
+france = (mempty :: StatePatch) { stateSeg = Just [ seg 0 5 white, seg 5 16 red, seg 16 23 white, seg 23 34 blue, seg 34 41 white, seg 41 52 blue, seg 52 57 white, seg 57 68 red, seg 68 101 white ]} where
+    red :: [Int]
+    red = [255,0,0]
+    white :: [Int]
+    white = [255,255,255]
+    blue :: [Int]
+    blue = [0,0,255]
+
+seg :: Int -> Int -> [Int] -> SegmentPatch
+seg start stop color = (mempty :: SegmentPatch) { segmentStart = Just start, segmentStop = Just stop, segmentCol = Just [color] }
--- src/Types.hs
+++ src/Types.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE UndecidableInstances     #-}
 
-module Types (State (..), Nightlight (..), StateComplete, StatePatch) where
+module Types (State (..), Nightlight (..), Segment (..), StateComplete, StatePatch, NightlightComplete, NightlightPatch, SegmentComplete, SegmentPatch) where
 
 import           Barbies.Bare
 import           Control.Applicative      (Alternative ((<|>)), empty)
@@ -30,24 +30,23 @@ data State t f f' = State
 --    , udpn :: Wear t f UdpNetwork
     , stateLor        :: Wear t f Int
     , stateMainseg    :: Wear t f Int
---    , seg :: Wear t f [Segment]
+    , stateSeg        :: Wear t f [Segment t f']
     } deriving stock (Generic) --, ConstraintsB, FunctorB, ApplicativeB)
 
 instance Functor f => FunctorB (State Covered f)
 instance FunctorB (State Bare f)
 
-instance ConstraintsB (State Bare f)
 instance FunctorT (State Covered)
 instance ApplicativeT (State Covered)
 
-deriving stock instance (AllBF Show f (State Bare f)) => Show (State Bare f f')
-deriving stock instance (AllBF Eq f (State Bare f)) => Eq (State Bare f f')
-deriving stock instance (Show (f Bool), Show (f Int), Show (f (Nightlight Covered f'))) => Show (State Covered f f')
-deriving stock instance (Eq (f Bool), Eq (f Int), Eq (f (Nightlight Covered f'))) => Eq (State Covered f f')
-deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Bare f f') instance (AllBF A.FromJSON f (State Bare f)) => A.FromJSON (State Bare f f')
-deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Bare f f') instance (AllBF A.ToJSON f (State Bare f)) => A.ToJSON (State Bare f f')
-deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Covered f f') instance (FromJSON (f Bool), FromJSON (f Int), FromJSON (f (Nightlight Covered f'))) => A.FromJSON (State Covered f f')
-deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Covered f f') instance (ToJSON (f Bool), ToJSON (f Int), ToJSON (f (Nightlight Covered f'))) => A.ToJSON (State Covered f f')
+deriving stock instance Show (State Bare f f')
+deriving stock instance Eq (State Bare f f')
+deriving stock instance (Show (f Bool), Show (f Int), Show (f (Nightlight Covered f')), Show (f [Segment Covered f'])) => Show (State Covered f f')
+deriving stock instance (Eq (f Bool), Eq (f Int), Eq (f (Nightlight Covered f')), Eq (f [Segment Covered f'])) => Eq (State Covered f f')
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Bare f f') instance A.FromJSON (State Bare f f')
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Bare f f') instance A.ToJSON (State Bare f f')
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Covered f f') instance (FromJSON (f Bool), FromJSON (f Int), FromJSON (f (Nightlight Covered f')), FromJSON (f [Segment Covered f'])) => A.FromJSON (State Covered f f')
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "state", ToLower]] (State Covered f f') instance (ToJSON (f Bool), ToJSON (f Int), ToJSON (f (Nightlight Covered f')), ToJSON (f [Segment Covered f'])) => A.ToJSON (State Covered f f')
 
 instance (Alternative f) => Semigroup (State Covered f f') where
   (<>) = tzipWith (<|>)
@@ -87,6 +86,61 @@ instance (Alternative f) => Semigroup (Nightlight Covered f) where
 instance (Alternative f) => Monoid (Nightlight Covered f) where
   mempty = bpure empty
 
+-- | Segment data type.
+type Segment :: Type -> (Type -> Type) -> Type
+data Segment t f = Segment
+    { segmentId :: Wear t f Int
+    , segmentStart :: Wear t f Int
+    , segmentStop :: Wear t f Int
+    , segmentLen :: Wear t f Int
+    , segmentGrp :: Wear t f Int
+    , segmentSpc :: Wear t f Int
+    , segmentOf :: Wear t f Int
+    , segmentOn :: Wear t f Bool
+    , segmentFrz :: Wear t f Bool
+    , segmentBri :: Wear t f Int
+    , segmentCct :: Wear t f Int
+    , segmentSet :: Wear t f Int
+    , segmentCol :: Wear t f [[Int]]
+    , segmentFx :: Wear t f Int
+    , segmentSx :: Wear t f Int
+    , segmentIx :: Wear t f Int
+    , segmentPal :: Wear t f Int
+    , segmentC1 :: Wear t f Int
+    , segmentC2 :: Wear t f Int
+    , segmentC3 :: Wear t f Int
+    , segmentSel :: Wear t f Bool
+    , segmentRev :: Wear t f Bool
+    , segmentMi :: Wear t f Bool
+    , segmentO1 :: Wear t f Bool
+    , segmentO2 :: Wear t f Bool
+    , segmentO3 :: Wear t f Bool
+    , segmentSi :: Wear t f Int
+    , segmentM12 :: Wear t f Int
+    } deriving stock (Generic)
+
+instance ConstraintsB (Segment Covered)
+instance FunctorB (Segment Covered)
+instance ApplicativeB (Segment Covered)
+
+instance ConstraintsB (Segment Bare)
+instance FunctorB (Segment Bare)
+
+deriving stock instance (AllBF Show f (Segment Bare)) => Show (Segment Bare f)
+deriving stock instance (AllBF Eq f (Segment Bare)) => Eq (Segment Bare f)
+deriving stock instance (AllBF Show f (Segment Covered)) => Show (Segment Covered f)
+deriving stock instance (AllBF Eq f (Segment Covered)) => Eq (Segment Covered f)
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "segment", ToLower]] (Segment Bare f) instance (AllBF A.FromJSON f (Segment Bare)) => A.FromJSON (Segment Bare f)
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "segment", ToLower]] (Segment Bare f) instance (AllBF A.ToJSON f (Segment Bare)) => A.ToJSON (Segment Bare f)
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "segment", ToLower]] (Segment Covered f) instance (AllBF A.FromJSON f (Segment Covered)) => A.FromJSON (Segment Covered f)
+deriving via CustomJSON '[OmitNothingFields, FieldLabelModifier '[StripPrefix "segment", ToLower]] (Segment Covered f) instance (AllBF A.ToJSON f (Segment Covered)) => A.ToJSON (Segment Covered f)
+
+instance (Alternative f) => Semigroup (Segment Covered f) where
+  (<>) = bzipWith (<|>)
+
+instance (Alternative f) => Monoid (Segment Covered f) where
+  mempty = bpure empty
+
 type ToLower :: Type
 data ToLower
 instance StringModifier ToLower where
@@ -98,3 +152,15 @@ type StateComplete = State Bare Identity Identity
 
 type StatePatch :: Type
 type StatePatch = State Covered Maybe Maybe
+
+type NightlightComplete :: Type
+type NightlightComplete = Nightlight Bare Identity
+
+type NightlightPatch :: Type
+type NightlightPatch = Nightlight Covered Maybe
+
+type SegmentComplete :: Type
+type SegmentComplete = Segment Bare Identity
+
+type SegmentPatch :: Type
+type SegmentPatch = Segment Covered Maybe
