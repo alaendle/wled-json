ffb7b88dbbca2fc06a0f7995159fae1de0bf901b
Andreas Ländle <alaendle@users.noreply.github.com>
2024-11-15 18:15:52 +0100

    Use quickcheck to prove monid laws.

--- test/TypesSpec.hs
+++ test/TypesSpec.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module TypesSpec (spec) where
 
@@ -6,7 +10,26 @@ import           Data.Aeson
 import           Data.Aeson.Key
 import           Data.Aeson.KeyMap
 import           Test.Hspec
+import           Test.QuickCheck
 import           Types
+
+instance Arbitrary StatePatch where
+  arbitrary = State <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance Arbitrary SegmentPatch where
+  arbitrary = Segment <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+                      <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+                      <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+                      <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance Arbitrary NightlightPatch where
+  arbitrary = Nightlight <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+prop_associativity :: StatePatch -> StatePatch -> StatePatch -> Bool
+prop_associativity a b c = (a <> b) <> c == a <> (b <> c)
+
+prop_identity :: StatePatch -> Bool
+prop_identity a = mempty <> a == a && a <> mempty == a
 
 spec :: Spec
 spec = do
@@ -16,3 +39,7 @@ spec = do
         let patch :: StatePatch = (mempty :: StatePatch) { stateBri = Just 1 }
         let json :: Either String Value = eitherDecode $ encode patch
         json `shouldBe` Right (Object (fromList [(fromString "bri",Number 1)]))
+      it "should satisfy the associativity law" $
+        property prop_associativity
+      it "should have an identity" $
+        property prop_identity
